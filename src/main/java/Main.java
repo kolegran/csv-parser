219856a3c576c69a7b438cd0eddcdf8832af191b
kolegran <kolegran@gmail.com>
2020-05-11 23:22:34 +0300

    Added final modifiers, simplified integer parsing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,20 +11,19 @@ public class Main {
 
     public static List<User> loadFromCsv(String csvFile) {
         try {
-            CSVReader reader = new CSVReader(new FileReader(csvFile), ',', '"', 1);
-            List<User> users = new ArrayList<>();
+            final CSVReader reader = new CSVReader(new FileReader(csvFile), ',', '"', 1);
+            final List<User> users = new ArrayList<>();
+
             String[] line;
             while ((line = reader.readNext()) != null) {
-                User user = new User();
-
+                final User user = new User();
                 user.setId(Integer.valueOf(line[0]));
                 user.setName(line[1]);
-                user.setAge(Integer.valueOf(line[2]));
-                user.setSex((Integer.parseInt(line[3]) != 0) ? true : false);
+                user.setAge(Integer.parseInt(line[2]));
+                user.setSex(Integer.parseInt(line[3]) != 0);
 
                 users.add(user);
             }
-
             return users;
         } catch (Exception e) {
             throw new IllegalStateException(e);
